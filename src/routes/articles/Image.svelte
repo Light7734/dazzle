95ca3ccf1a6058b36cdcf847dc5f43ddfafef1ef
light7734 <light7734@tuta.io>
2025-05-06 15:14:19 +0330

    wip

---
+++ src/routes/articles/Image.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let paths: string[] = [];
+	export let caption: string = '';
+</script>
+
+<div class="container">
+		{#each paths as path (path)}
+			<img src={path} alt="" />
+		{/each}
+</div>
+
+<style>
+	.container {
+        width: fit-content;
+        margin-left: auto;
+        margin-right: auto;
+		background-color: #1d2021;
+		padding: 1em;
+		border: 1px solid #928374;
+		border-radius: 2px;
+		display: flex; /* Arrange images horizontally */
+		/* Removed flex properties from here */
+        gap: 1em;
+	}
+
+</style>
